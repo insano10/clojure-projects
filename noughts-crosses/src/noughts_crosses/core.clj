047518f66cf27e2a0602999932c26d412801fc56
jennifer.beckett <jennifer.beckett@ovoenergy.com>
2018-08-28 10:41:24 +0100

    remove initial-grid and simplify tests

--- noughts-crosses/src/noughts_crosses/core.clj
+++ noughts-crosses/src/noughts_crosses/core.clj
@@ -1,10 +1,6 @@
 (ns noughts-crosses.core
   (:gen-class))
 
-(defn initial-grid
-  []
-  [:- :- :-])
-
 (defn move
   [grid pos plyr]
   {:pre [(<= 0 pos 2), (= :- (nth grid pos))]}
@@ -14,7 +10,4 @@
   [grid plyr]
   (= (count grid) (count (filter #(= % plyr) grid))))
 
-(defn play-fn
-  []
-  (let [grid (atom (initial-grid))]
-    (fn [pos plyr] (do (swap! grid move pos plyr)))))
+
--- noughts-crosses/test/noughts_crosses/core_test.clj
+++ noughts-crosses/test/noughts_crosses/core_test.clj
@@ -5,7 +5,7 @@
 (deftest move-test
   (testing "Play game and check X wins and O loses"
     (let [grid
-          (-> (initial-grid)
+          (-> [:- :- :-]
               (move 0 :X)
               (move 1 :X)
               (move 2 :X))]
@@ -15,7 +15,11 @@
 
 (deftest move-illegal-positions-test
   (testing "Check illegal moves are rejected"
-    (let [f (play-fn) grid (do (f 0 :X) (f 1 :X) (f 2 :O))]
+    (let [grid
+          (-> [:- :- :-]
+              (move 0 :X)
+              (move 1 :X)
+              (move 2 :X))]
       (is (thrown? AssertionError (move grid 2 :X)))
       (is (thrown? AssertionError (move grid -1 :X)))
       (is (thrown? AssertionError (move grid 3 :X))))))
